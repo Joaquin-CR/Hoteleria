09b4d0fe1102b51675d38d66ae26c16592d4e913
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-06 23:25:01 -0600

    Enrutamiento e importaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// COMPONENTES
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'landingpage', pathMatch:'full'}, //Esto va a ser lo primero que se mostrará cuando no haya nada cargado
+  {path: 'landingpage', component: LandingPageComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
