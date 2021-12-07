7231d2da098d5af4112014cf02d4cc5dc7bb19fd
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-07 13:54:39 -0600

    Se agregaron los componentes de login tanto de usuario como de empleados

---
+++ src/app/Components/login-empleado/login-empleado.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginEmpleadoComponent } from './login-empleado.component';
+
+describe('LoginEmpleadoComponent', () => {
+  let component: LoginEmpleadoComponent;
+  let fixture: ComponentFixture<LoginEmpleadoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginEmpleadoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginEmpleadoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 import { MenuComponent } from './Components/menu/menu.component';
+import { LoginUsuarioComponent } from './Components/login-usuario/login-usuario.component';
+import { LoginEmpleadoComponent } from './Components/login-empleado/login-empleado.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'landingpage', pathMatch:'full'}, //Esto va a ser lo primero que se mostrará cuando no haya nada cargado
   {path: 'landingpage', component: LandingPageComponent},
+  {path: 'login', component: LoginUsuarioComponent},
+  {path: 'acceso-empleado', component: LoginEmpleadoComponent},
   {path:'**', redirectTo: 'landingpage', pathMatch: 'full'} //Si se ingresa un complemento de liga erroneo esta redirecciona a la indicada
 ]
 
@@ -25,7 +29,9 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LandingPageComponent,
-    MenuComponent
+    MenuComponent,
+    LoginUsuarioComponent,
+    LoginEmpleadoComponent
   ],
   imports: [
     CommonModule,
