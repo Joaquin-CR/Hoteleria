c8fdc3097d3ae5201b01c51113d611ff224d1559
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-07 10:58:24 -0600

    Modificaciones en la landing page

--- src/app/Components/landing-page/landing-page.component.ts
+++ src/app/Components/landing-page/landing-page.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit 
+{
+  nuevoRegistro: FormGroup;
+  submitted = false;
+  loading = false;
+  constructor(private fb: FormBuilder,) 
+  { 
+    this.nuevoRegistro = this.fb.group({
+      fecIn: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      fecOut: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumA: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumN: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumC: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]]
+    })
+  }
 
-  constructor() { }
+  ngOnInit(): void 
+  {
+    // 
+  }
 
-  ngOnInit(): void {
+  agregarRegistro()
+  {
+    this.loading = true;
+    this.submitted = true;
+    // if(this._cookies.checkToken())
+    // {
+    //   console.log(this.nuevoRegistro);
+    //   // this._registroService.getReservaciones().subscribe(res =>{
+    //   //   console.log(res)
+    //   // },
+    //   //   err => console.log(err)
+    //   // )
+    //   const nuevaRes: Reservacion = 
+    //   {
+    //     idRes: "zxy8888",
+    //     idUser: 1999,
+    //     fecIn: this.nuevoRegistro.value.fecIn,
+    //     fecOut: this.nuevoRegistro.value.fecOut,
+    //     numA: this.nuevoRegistro.value.NumA,
+    //     numN: this.nuevoRegistro.value.NumN,
+    //     numC: this.nuevoRegistro.value.NumC,
+    //   }
+    //   this._registroService.addReservacion(nuevaRes).subscribe();
+      
+    //   this._registroService.getReservaciones().subscribe(res =>{
+    //     console.log(res)
+    //   },
+    //     err => console.log(err)
+    //   )
+    // }
   }
 
 }
