e1dfd9a4490d6a8cd85ed1f74ab1824c7f593b23
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-08 00:19:37 -0600

    Maquetación del registro

--- src/app/Components/registro-usuarios/registro-usuarios.component.ts
+++ src/app/Components/registro-usuarios/registro-usuarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-usuarios',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroUsuariosComponent implements OnInit {
 
-  constructor() { }
+  registroUsuario: FormGroup;
+  submitted = false;
+  loading = false;
 
-  ngOnInit(): void {
+  nombre: string;
+  apellido: string;
+  user: string;
+  correo: string;
+  contrasena: string;
+  confirm_contrasena: string;
+  telefono: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router) 
+  {
+    this.registroUsuario = this.fb.group({
+      Name: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
+      LastName: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
+      UserName: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      Mail: ['', Validators.email],
+      Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      ConfrimPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      Phone: ['', [Validators.required, Validators.pattern("^[0-9]*$")]]
+    })
+
+    this.nombre = "";
+    this.apellido = "";
+    this.user = "";
+    this.correo = "";
+    this.contrasena = "";
+    this.confirm_contrasena = "";
+    this.telefono = "";
+  }
+
+  ngOnInit(): void 
+  {
+    // 
+  }
+
+  nuevoRegistro()
+  {
+    this. contrasena = this.registroUsuario.value.Password;
+    this.confirm_contrasena = this.registroUsuario.value.ConfrimPass;
+    if(this.contrasena == this.confirm_contrasena)
+    {
+      this.nombre = this.registroUsuario.value.Name;
+      this.apellido = this.registroUsuario.value.LastName;
+      this.user = this.registroUsuario.value.UserName;
+      this.correo = this.registroUsuario.value.Mail;
+      this.telefono = this.registroUsuario.value.Phone;
+    }
+    else
+    {
+      // 
+    }
+    console.log("Si jala");
   }
 
 }
