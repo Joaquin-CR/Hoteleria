9ed6c3075db87c47f5b74ef4776cf4a0715053f7
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-07 18:08:09 -0600

    Se maquetaron los login de usuarios y empleados

--- src/app/Components/login-empleado/login-empleado.component.ts
+++ src/app/Components/login-empleado/login-empleado.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-empleado',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginEmpleadoComponent implements OnInit {
 
-  constructor() { }
+  loginE: FormGroup;
+  submitted = false;
+  loading = false;
+  us: string;
+  pass: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              //private _empSession: EmpleadosesionService,
+              private toastr: ToastrService) 
+  { 
+    this.loginE = this.fb.group({
+      User: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]]
+    });
+
+    this.us = "Joaquin";
+    this.pass = "1234";
+  }
 
   ngOnInit(): void {
+  }
+
+  empleadoLogin()
+  {
+    let formusu = this.loginE.value.User;
+    let formpass = this.loginE.value.Password;
+    if(formusu == this.us && formpass == this.pass)
+    {
+      this.toastr.success('Acceso concedido', 'Acción exitosa',
+      {
+        positionClass: 'toast-bottom-right'
+      });
+      //Se genera la cookie
+      //this._cookies.setToken(this._cookies.getRandomToken(16), this._cookies.nuevaExpiracion(1));
+      //this._empSession.loginEmpleado();
+      this.router.navigate(['/empleados']);
+    }
+    else
+    {
+      this.toastr.error('Acceso denegado', 'ERROR',
+      {
+        positionClass: 'toast-bottom-right'
+      });
+      // console.log("Acceso denegado");
+    }
   }
 
 }
--- src/app/Components/login-usuario/login-usuario.component.ts
+++ src/app/Components/login-usuario/login-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-usuario',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginUsuarioComponent implements OnInit {
 
-  constructor() { }
+  login: FormGroup;
+  submitted = false;
+  loading = false;
+  usuario: string;
+  contra: string;
+
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router,) 
+{ 
+  this.login = this.fb.group({
+    User: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    checkTerminons: ['', Validators.required]
+  });
+
+  this.usuario = "Joaquin13";
+  this.contra = "1234";
+}
 
   ngOnInit(): void {
+  }
+
+  usuarioLogin()
+  {
+    this.loading = true;
+    this.submitted = true;
+    let formusu = this.login.value.User;
+    let formpass = this.login.value.Password;
+    if(formusu == this.usuario && formpass == this.contra)
+    {
+      // Se genera la cookie
+      // this._cookies.setToken(this._cookies.getRandomToken(16), this._cookies.nuevaExpiracion(1));
+
+      this.toastr.success('Acceso concedido', 'Acción exitosa',
+      {
+        positionClass: 'toast-bottom-right'
+      });
+      // Se redirecciona de nuevo a la landing
+      //this.router.navigate(['/landingpage']);
+    }
   }
 
 }
