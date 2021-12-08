6e3cf01e82d7793871f48b1d9c23f40a044800a9
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-07 23:45:27 -0600

    Se configuró el enlace de los módulos con el app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: AppComponent,
+    children: [
+      {
+        path: 'empleados',
+        loadChildren: () => import('./Modules/empleados/empleados.module').then(mod => mod.EmpleadosModule)
+      },
+      {
+        path: 'usuarios',
+        loadChildren: () => import('./Modules/usuarios/usuarios.module').then(mod => mod.UsuariosModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
