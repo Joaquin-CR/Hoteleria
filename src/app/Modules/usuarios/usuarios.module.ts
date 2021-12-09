b0d5613ae39335f7ccdf79ef422e53b5ef33c4aa
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-08 18:14:44 -0600

    Se creo el componente para el NavBar en el módulo de usuarios

--- src/app/Modules/usuarios/usuarios.module.ts
+++ src/app/Modules/usuarios/usuarios.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { UsuariosComponent } from './usuarios.component';
+import { MenuUsuarioComponent } from './Component/menu-usuario/menu-usuario.component';
 
 
 @NgModule({
   declarations: [
-    UsuariosComponent
+    UsuariosComponent,
+    MenuUsuarioComponent
   ],
   imports: [
     CommonModule,
