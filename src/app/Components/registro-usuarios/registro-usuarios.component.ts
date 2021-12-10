9155081354a90e75e09c9cff6c0e90e4feab0c6e
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 12:05:57 -0600

    Se elaboró las validaciones para registrarse como usuario

--- src/app/Components/registro-usuarios/registro-usuarios.component.ts
+++ src/app/Components/registro-usuarios/registro-usuarios.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Usuario, UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-registro-usuarios',
@@ -13,16 +15,16 @@ export class RegistroUsuariosComponent implements OnInit {
   submitted = false;
   loading = false;
 
-  nombre: string;
-  apellido: string;
-  user: string;
-  correo: string;
+  userName: string;
+  phone: string;
   contrasena: string;
   confirm_contrasena: string;
-  telefono: string;
+
 
   constructor(private fb: FormBuilder,
-              private router: Router) 
+              private router: Router,
+              private toastr: ToastrService,
+              private _usuarioService: UsuariosService) 
   {
     this.registroUsuario = this.fb.group({
       Name: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
@@ -30,17 +32,15 @@ export class RegistroUsuariosComponent implements OnInit {
       UserName: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
       Mail: ['', Validators.email],
       Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
-      ConfrimPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      ConfirmPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      Lada: ['', Validators.required],
       Phone: ['', [Validators.required, Validators.pattern("^[0-9]*$")]]
     })
 
-    this.nombre = "";
-    this.apellido = "";
-    this.user = "";
-    this.correo = "";
+    this.userName = "";
     this.contrasena = "";
     this.confirm_contrasena = "";
-    this.telefono = "";
+    this.phone = "";
   }
 
   ngOnInit(): void 
@@ -50,21 +50,47 @@ export class RegistroUsuariosComponent implements OnInit {
 
   nuevoRegistro()
   {
-    this. contrasena = this.registroUsuario.value.Password;
-    this.confirm_contrasena = this.registroUsuario.value.ConfrimPass;
+    this.contrasena = this.registroUsuario.value.Password;
+    this.confirm_contrasena = this.registroUsuario.value.ConfirmPass;
     if(this.contrasena == this.confirm_contrasena)
     {
-      this.nombre = this.registroUsuario.value.Name;
-      this.apellido = this.registroUsuario.value.LastName;
-      this.user = this.registroUsuario.value.UserName;
-      this.correo = this.registroUsuario.value.Mail;
-      this.telefono = this.registroUsuario.value.Phone;
+      this.userName = this.registroUsuario.value.UserName;
+      this._usuarioService.getUsuario(this.userName).subscribe(res =>{
+        if(Object.keys(res).length == 0)
+        {
+          this.phone = ""+this.registroUsuario.value.Lada+this.registroUsuario.value.Phone
+          const nuevoUser: Usuario = 
+          {
+            nomUser: this.registroUsuario.value.Name,
+            apellUser: this.registroUsuario.value.LastName,
+            userN: this.userName,
+            mail: this.registroUsuario.value.Mail,
+            pass: this.contrasena,
+            cell: this.phone
+          }
+          this._usuarioService.addUsuario(nuevoUser).subscribe();
+          this.toastr.success('El registro se logró de manera exitosa', 'ACCION EXITOSA',
+          {
+            positionClass: 'toast-bottom-right'
+          });
+          this.router.navigate(['/login']);
+        }
+        else
+        {
+          this.toastr.warning('El nombre de usuario ya está ocupado, favor de ingresar otro', 'ADVERTENCIA',
+          {
+            positionClass: 'toast-bottom-right'
+          });
+        }
+      });
     }
     else
     {
-      // 
+      this.toastr.warning('Las contraseñas no coinciden', 'ADVERTENCIA',
+      {
+        positionClass: 'toast-bottom-right'
+      });
     }
-    console.log("Si jala");
   }
 
 }
