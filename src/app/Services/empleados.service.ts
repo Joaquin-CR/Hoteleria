0b41022ddf537723b603e82f108238b92ba24c2a
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 15:47:36 -0600

    Se añadieron los métodos para hacer las consultas al servidor

--- src/app/Services/empleados.service.ts
+++ src/app/Services/empleados.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class EmpleadosService 
 {
 
-  url = "/apiEmpleados";
+  url = "/api/empleados";
 
   constructor(private _http: HttpClient) 
   { 
@@ -18,4 +18,34 @@ export class EmpleadosService
   {
     return this._http.get(this.url);
   }
+
+  addEmpleadp(empleado: Empleado) //Mete los valores que se encuentran en el export de Reservaciones
+  {
+    return this._http.post(this.url+'/', empleado);
+  }
+
+  getempleado(idEmpleado:string)
+  {
+    return this._http.get(this.url+'/'+idEmpleado);
+  }
+
+  deleteEmpleado(idEmpleado:string)
+  {
+    return this._http.delete(this.url+'/'+idEmpleado);
+  }
+
+  updateEmpleado(id:string, empleado: Empleado)
+  {
+    return this._http.put(this.url+'/'+id, empleado);
+  }
+}
+
+export interface Empleado
+{
+  id: number;
+  nomEmpleado: string;
+  appellEmpleado: string;
+  mail: string;
+  pass: string;
+  puesto: string;
 }
