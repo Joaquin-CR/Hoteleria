b2de07d2577f21a34ced67f7aefb6383f8ad0d68
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 01:27:23 -0600

    Se empezó a maquetar el registro de empleados

--- src/app/Modules/empleados/Components/registro-empleados/registro-empleados.component.ts
+++ src/app/Modules/empleados/Components/registro-empleados/registro-empleados.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-empleados',
   templateUrl: './registro-empleados.component.html',
   styleUrls: ['./registro-empleados.component.css']
 })
-export class RegistroEmpleadosComponent implements OnInit {
+export class RegistroEmpleadosComponent implements OnInit 
+{
 
-  constructor() { }
+  registroEmpleado: FormGroup;
+  submitted = false;
+  loading = false;
+
+  id: number;
+  puesto: string;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  contrasena: string;
+  confirm_contrasena: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router) 
+  { 
+    this.registroEmpleado = this.fb.group({
+      Name: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
+      LastName: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
+      Cargo: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*$")]],
+      Mail: ['', Validators.email],
+      Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      ConfrimPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    });
+    this.id = Math.random() * (2000000 - 1) + 1;
+    this.puesto = "";
+    this.nombre = "";
+    this.apellido = "";
+    this.correo = "";
+    this.contrasena = "";
+    this.confirm_contrasena = "";
+
+  }
 
   ngOnInit(): void {
+  }
+
+  nuevoEpmleado()
+  {
+    // 
   }
 
 }
