ae6ffc4a161949e5af9961f8251ec3c201ec0383
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 00:52:42 -0600

    Se añadió la ruta para el formulario de registro para empleados

--- src/app/Modules/empleados/empleados-routing.module.ts
+++ src/app/Modules/empleados/empleados-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GestionEmpleadosComponent } from './Components/gestion-empleados/gestion-empleados.component';
+import { RegistroEmpleadosComponent } from './Components/registro-empleados/registro-empleados.component';
 import { EmpleadosComponent } from './empleados.component';
 
 const routes: Routes = [
   { path: '', component: EmpleadosComponent },
-  { path: 'gestion-empleados', component: GestionEmpleadosComponent }
+  { path: 'gestion-empleados', component: GestionEmpleadosComponent },
+  { path: 'registro-empleados', component: RegistroEmpleadosComponent}
 ];
 
 @NgModule({
