7858c11e1d50440a09b489a043ada52d825e4da2
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 15:10:04 -0600

    Se modifico el navbar para poder cerrar sesión desde la landing

--- src/app/Components/menu/menu.component.ts
+++ src/app/Components/menu/menu.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit 
+{
 
-  constructor() { }
+  logeado: boolean = this._usuarioService.logeado 
+  constructor(private _usuarioService: UsuariosService,
+              private router: Router,) 
+  { 
+    // 
+  }
 
   ngOnInit(): void {
+  }
+
+  cerrarSesion()
+  {
+    this._usuarioService.usuarioLogeado(false);
+    this.router.navigate(['/landingpage']);
   }
 
 }
