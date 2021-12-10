e5cb2cb188984a21406f4c5dc22daa90b893e35b
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-10 15:18:21 -0600

    Se hace la consulta al servidor
    Falta extraer los datos

--- src/app/Components/login-usuario/login-usuario.component.ts
+++ src/app/Components/login-usuario/login-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-login-usuario',
@@ -13,12 +14,13 @@ export class LoginUsuarioComponent implements OnInit {
   login: FormGroup;
   submitted = false;
   loading = false;
-  usuario: string;
+  userName: string;
   contra: string;
 
   constructor(private fb: FormBuilder,
-    private toastr: ToastrService,
-    private router: Router,) 
+              private toastr: ToastrService,
+              private router: Router,
+              private _usuarioService: UsuariosService) 
 { 
   this.login = this.fb.group({
     User: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
@@ -26,8 +28,8 @@ export class LoginUsuarioComponent implements OnInit {
     checkTerminons: ['', Validators.required]
   });
 
-  this.usuario = "Joaquin13";
-  this.contra = "1234";
+  this.userName = "";
+  this.contra = "";
 }
 
   ngOnInit(): void {
@@ -37,27 +39,43 @@ export class LoginUsuarioComponent implements OnInit {
   {
     this.loading = true;
     this.submitted = true;
-    let formusu = this.login.value.User;
-    let formpass = this.login.value.Password;
-    if(formusu == this.usuario && formpass == this.contra)
-    {
-      // Se genera la cookie
-      // this._cookies.setToken(this._cookies.getRandomToken(16), this._cookies.nuevaExpiracion(1));
-
-      this.toastr.success('Acceso concedido', 'Acción exitosa',
+    this.userName = this.login.value.User;
+    this.contra = this.login.value.Password;
+    this._usuarioService.getUsuario(this.userName).subscribe(res =>{
+      if(Object.keys(res).length == 0)
       {
-        positionClass: 'toast-bottom-right'
-      });
-      // Se redirecciona de nuevo a la landing
-      this.router.navigate(['/landingpage']);
-    }
-    else
-    {
-      this.toastr.error('El usuario o la contraseña son incorrectas', 'Error',
+        this.toastr.warning('No se encontró registro del usuario', 'ADVERTENCIA',
+        {
+          positionClass: 'toast-bottom-right'
+        });
+        // this.router.navigate(['/login']);
+      }
+      else
       {
-        positionClass: 'toast-bottom-right'
-      });
-    }
+        this._usuarioService.usuarioLogeado(true);
+        this.router.navigate(['/landingpage']);
+        console.log(res);
+        // if(formusu == this.userName && formpass == this.contra)
+        // {
+        //   this.logeado = true;
+        //   // Se genera la cookie
+        //   // this._cookies.setToken(this._cookies.getRandomToken(16), this._cookies.nuevaExpiracion(1));
+        //   this.toastr.success('Acceso concedido', 'Acción exitosa',
+        //   {
+        //     positionClass: 'toast-bottom-right'
+        //   });
+        //   // Se redirecciona de nuevo a la landing
+        //   this.router.navigate(['/landingpage']);
+        // }
+        // else
+        // {
+        //   this.toastr.error('El usuario o la contraseña son incorrectas', 'Error',
+        //   {
+        //     positionClass: 'toast-bottom-right'
+        //   });
+        // } 
+      }
+    });
   }
 
 }
