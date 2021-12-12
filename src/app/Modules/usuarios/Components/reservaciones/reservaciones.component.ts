1f7f031379a0629bbb496156f59993d68eee5f0e
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-12 02:13:11 -0600

    Intento de mostrar los datos extraidos de la base de datos

--- src/app/Modules/usuarios/Components/reservaciones/reservaciones.component.ts
+++ src/app/Modules/usuarios/Components/reservaciones/reservaciones.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ReservacionesService } from 'src/app/Services/reservaciones.service';
+import { UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-reservaciones',
@@ -7,10 +10,44 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ReservacionesComponent implements OnInit {
 
-  @Input() llamarMiRes!: boolean;
-  constructor() { }
+  idUser!: string;
+  res!: any[][];
 
-  ngOnInit(): void {
+  @Input() llamarMiRes!: boolean;
+  constructor(private _reservacionService: ReservacionesService,
+              private toastr: ToastrService,
+              // private router: Router,
+              private _usuarioService: UsuariosService) 
+  { 
+    // 
   }
 
+  ngOnInit(): void 
+  {
+    // 
+  }
+
+  setIdUser()
+  {
+    this.idUser = this._usuarioService.getIdUsuario();
+  }
+
+  getReservaciones()
+  {
+    this._reservacionService.obtenerMiReservacion("1").subscribe(data =>{
+      if(Object.keys(data).length == 0)
+      {
+        let nota = "No cuenta aun con una reservación"
+      }
+      else
+      {
+        var reservaciones = Object.values(data);
+        console.log(reservaciones[0]['id_res']);
+        // reservaciones.forEach((element: any) => {
+        //   this.res.push({id: element['id_res']})});
+        // console.log(this.res);
+        console.log(reservaciones);
+      }
+    });
+  }
 }
