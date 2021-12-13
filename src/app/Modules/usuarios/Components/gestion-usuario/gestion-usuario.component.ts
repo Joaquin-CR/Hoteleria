d29358fd30d8bbd43a2930eeada79fd74ab1c19c
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-13 16:06:24 -0600

    Se hicieron los ajustes necesarios (se añadieron métodos y validaciones) para la nueva función de cambiar la contraseña

--- src/app/Modules/usuarios/Components/gestion-usuario/gestion-usuario.component.ts
+++ src/app/Modules/usuarios/Components/gestion-usuario/gestion-usuario.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { UsuariosService } from 'src/app/Services/usuarios.service';
+import { UpdateUser, UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-gestion-usuario',
@@ -12,14 +12,19 @@ export class GestionUsuarioComponent implements OnInit {
 
   modificarUsuario: FormGroup;
   modificarContra: FormGroup;
-  nombre = "Joaquin";
-  apellido = "Carreño";
-  nombreUser = "JJ";
-  correo = "joaquin@gmail.com";
-  lada: string = "+52";
-  phone = "2228634123";
-  telefono = "+522228634123";
   editar = false;
+  newPass = false;
+
+  idUser: string = "1";
+  nombre: string = "Joaquin";
+  apellido: string = "Carreño";
+  nombreUser: string = "JJ";
+  newUserN!: string;
+  correo: string = "joaquin@gmail.com";
+  lada: string = "+52";
+  phone: string = "2228634123";
+  telefono: string = "+522228634123";
+
   @Input() llamarConfigUser!: boolean;
   constructor(private fb: FormBuilder,
               private toastr: ToastrService,
@@ -34,6 +39,7 @@ export class GestionUsuarioComponent implements OnInit {
       Phone: ['', [Validators.required, Validators.pattern("^[0-9]*$")]]
     });
     this.modificarContra = this.fb.group({
+      OldPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
       Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
       ConfirmPass: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
     });
@@ -55,9 +61,50 @@ export class GestionUsuarioComponent implements OnInit {
     this.mostrarDatos();
   }
 
+  cambiarPass()
+  {
+    this.newPass = true;
+  }
+
   cancelarUpdate()
   {
     this.editar = false;
+    this.newPass = false;
+  }
+
+  verificarUpdate()
+  {
+
+    this._usuarioService.getUsuario(this.nombreUser).subscribe(res =>{
+      /* EL NOMBRE DE USUARIO DEBE DE SER UNICO, ASÍ QUE SI NO ENCUENTRA UNO EXISTENTE ENTRA DEL LADO 
+         VERDADERO DEL IF*/
+      if(Object.keys(res).length == 0)
+      {
+        this.phone = ""+this.modificarUsuario.value.Lada+this.modificarUsuario.value.Phone
+        const nuevoUser: UpdateUser
+        = 
+        {
+          nomUser: this.modificarUsuario.value.Name,
+          apellUser: this.modificarUsuario.value.LastName,
+          userN: this.modificarUsuario.value.LastName,
+          mail: this.modificarUsuario.value.Mail,
+          cell: this.phone
+        }
+        // SE REGISTRAN LAS MODIFICACIONES DEL USUARIO EN LA BASE DE DATOS
+        // this._usuarioService.addUsuario(nuevoUser).subscribe();
+        this.toastr.success('El registro se logró de manera exitosa', 'ACCION EXITOSA',
+        {
+          positionClass: 'toast-bottom-right'
+        });
+      }
+      else
+      {
+        this.toastr.warning('El nombre de usuario ya está ocupado, favor de ingresar otro', 'ADVERTENCIA',
+        {
+          positionClass: 'toast-bottom-right'
+        });
+      }
+    });
   }
 
   mostrarDatos()
@@ -71,5 +118,4 @@ export class GestionUsuarioComponent implements OnInit {
       Phone: this.phone
     });
   }
-  // this.crearLibro.setValue({})
 }
