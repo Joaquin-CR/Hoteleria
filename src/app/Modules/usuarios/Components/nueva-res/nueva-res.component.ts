dcf05154b621f325b72bf221cb81e5a622a8268c
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-13 16:23:08 -0600

    Se añadió el método para extraer la información del HTML

--- src/app/Modules/usuarios/Components/nueva-res/nueva-res.component.ts
+++ src/app/Modules/usuarios/Components/nueva-res/nueva-res.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nueva-res',
@@ -7,10 +8,47 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NuevaResComponent implements OnInit {
 
+  nuevoRegistro: FormGroup;
+
   @Input() llamarNewRes!: boolean;
-  constructor() { }
+  constructor(private fb: FormBuilder,) 
+  { 
+    this.nuevoRegistro = this.fb.group({
+      fecIn: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      fecOut: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumA: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumN: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]],
+      NumC: ['', [Validators.required, Validators.pattern("^[0-9-]*$")]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  agregarRegistro()
+  {
+    const  generateRandomString = () => {
+      const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let result1= ' ';
+      const charactersLength = characters.length;
+      for ( let i = 0; i < 7; i++ ) {
+          result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+  
+      return result1;
+    }
+
+    let idReservacion = ""+generateRandomString();
+    // const nuevaRes: Reservacion = 
+    //   {
+    //     idRes: idReservacion,
+    //     idUser: 1999,
+    //     fecIn: this.nuevoRegistro.value.fecIn,
+    //     fecOut: this.nuevoRegistro.value.fecOut,
+    //     numA: this.nuevoRegistro.value.NumA,
+    //     numN: this.nuevoRegistro.value.NumN,
+    //     numC: this.nuevoRegistro.value.NumC,
+    //   }
+    //   this._registroService.addReservacion(nuevaRes).subscribe();
+  }
 }
