2f6d7434167f8326e49ec356be83ac31086f5c60
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-14 15:39:13 -0600

    Se eliminó el componente de supervisor

--- src/app/Modules/empleados/empleados.module.ts
+++ src/app/Modules/empleados/empleados.module.ts
@@ -3,20 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { EmpleadosRoutingModule } from './empleados-routing.module';
 import { EmpleadosComponent } from './empleados.component';
-import { SupervisorComponent } from './Components/supervisor/supervisor.component';
 import { GestionEmpleadosComponent } from './Components/gestion-empleados/gestion-empleados.component';
 import { MenuEmpleadoComponent } from './Components/menu-empleado/menu-empleado.component';
 import { RegistroEmpleadosComponent } from './Components/registro-empleados/registro-empleados.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReservacionesComponent } from './Components/reservaciones/reservaciones.component';
 
 
 @NgModule({
   declarations: [
     EmpleadosComponent,
-    SupervisorComponent,
     GestionEmpleadosComponent,
     MenuEmpleadoComponent,
-    RegistroEmpleadosComponent
+    RegistroEmpleadosComponent,
+    ReservacionesComponent
   ],
   imports: [
     CommonModule,
