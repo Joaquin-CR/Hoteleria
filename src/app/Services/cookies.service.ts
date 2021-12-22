649ecf89bfce53fc170273a9efebf1ef98a42456
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-22 09:20:14 -0600

    Se configuró el servicio de las cookies

--- src/app/Services/cookies.service.ts
+++ src/app/Services/cookies.service.ts
@@ -1,10 +1,80 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CookiesService {
+export class CookiesService 
+{
 
-  constructor() 
-  { }
+  existenciaCookie: boolean;
+
+  constructor(private router: Router,
+              private toastr: ToastrService,
+              private _cookies: CookieService,) 
+  { 
+    this.existenciaCookie = false;
+  }
+
+  setToken(token: string, minutos: number)
+  {
+    this._cookies.set("token", token, minutos);
+  }
+
+  getToken()
+  {
+    return this._cookies.get("token");
+  }
+
+  getRandomToken(size: number)
+  {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for (var i = 0; i < size; i++)
+    {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    // console.log(result);
+    return result;
+  }
+
+  Logout()
+  {
+    // console.log('Borrando');
+
+    //SE DEBE CHECAR EN QUE PARTE DE LA PÁGUINA ESTA PARA REDIRECCIONARLA
+    this.router.navigate(['/landing']);
+    /********************************** */
+
+    this._cookies.delete("token", "/");
+  }
+
+  nuevaExpiracion(minutos: number)
+  {
+    return (1/1440)*minutos;
+  }
+
+  checkToken()
+  {
+    let cookie = this.getToken();
+    if(!cookie)
+    {
+      this.Logout();
+      // CHECAR PARA CUANDO SE HAGA UNA RESERVACION DESDE LA LANDINGPAGE
+      this.toastr.warning('Favor de iniciar sesion antes de ejecutar cualquier acción', 'ADVERTENCIA',
+          {
+            positionClass: 'toast-bottom-right'
+          });
+      this.existenciaCookie = false;
+    }
+    else
+    {
+      this.setToken(this.getToken(), this.nuevaExpiracion(2));
+      // console.log("Token renovado");
+      this.existenciaCookie = true;
+    }
+    return cookie;
+  }
 }
