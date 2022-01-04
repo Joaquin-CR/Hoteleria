7e53f86fb453c77908c517e357197cd8b342b3aa
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-01-04 00:27:36 -0600

    Se programó los guards tanto para usuarios como para empleados

--- src/app/Guards/login-empleado.guard.ts
+++ src/app/Guards/login-empleado.guard.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
+import { EmpleadosService } from '../Services/empleados.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginEmpleadoGuard implements CanActivate {
+  constructor(private router: Router,
+              private _empSession: EmpleadosService,
+              private toastr: ToastrService,)
+{
+// 
+}
+  redirect(acceso: boolean): any{
+    if(!acceso)
+    {
+      this.router.navigate(['/', 'acceso-empleado'])
+      this.toastr.error('Acceso denegado', 'ERROR',
+      {
+        positionClass: 'toast-bottom-right'
+      });
+    }
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    let cookieVigente = this._empSession.statusSesion();
+    this.redirect(cookieVigente);
+    return cookieVigente;
   }
   
 }
--- src/app/Guards/login-users.guard.ts
+++ src/app/Guards/login-users.guard.ts
@@ -1,15 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { UsuariosService } from '../Services/usuarios.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginUsersGuard implements CanActivate {
+
+  constructor(private router: Router,
+                private _userService: UsuariosService,
+                private toastr: ToastrService,)
+  {
+    // 
+  }
+
+  redirect(acceso: boolean): any{
+    if(!acceso)
+    {
+      this.router.navigate(['/', 'login'])
+      this.toastr.error('Acceso denegado', 'ERROR',
+      {
+        positionClass: 'toast-bottom-right'
+      });
+    }
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      let cookieVigente = this._userService.statusSesion();
+      this.redirect(cookieVigente);
+    return cookieVigente;
   }
   
 }
