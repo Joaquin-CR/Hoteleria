aa44d4b6ecfebf302585d5a7b503a7cbaf7e9112
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-01-07 20:35:47 -0600

    Se implementaron los elementos y funciones para la directiva

--- src/app/Directives/imagen-rota.directive.ts
+++ src/app/Directives/imagen-rota.directive.ts
@@ -11,6 +11,11 @@ export class ImagenRotaDirective
     // 
   }
 
-  
+  @HostListener('error')
+  cargarImagenPorDefecto()
+  {
+    const elemnt = this.elementRef.nativeElement;
+    elemnt.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8uia93jUhtlaNB5s9-xnmhh8tek0NsQ0BMg&usqp=CAU';
+  }
 
 }
