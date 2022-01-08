7461cb373883c0b2863a226f1069446bac44ef91
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-01-07 22:55:33 -0600

    Se añadió la funcionalidad del Pipe para el precio

--- src/app/Components/landing-page/landing-page.component.ts
+++ src/app/Components/landing-page/landing-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CookiesService } from 'src/app/Services/cookies.service';
 import { EmpleadosService } from 'src/app/Services/empleados.service';
-import { ReservacionesService } from 'src/app/Services/reservaciones.service';
+import { Reservacion, ReservacionesService } from 'src/app/Services/reservaciones.service';
 import { UsuariosService } from 'src/app/Services/usuarios.service';
 // import { CookiesService } from 'src/app/Services/cookies-s.service';
 
@@ -15,11 +16,15 @@ export class LandingPageComponent implements OnInit
   nuevoRegistro: FormGroup;
   submitted = false;
   loading = false;
+
+  tipo!: string;
+  salario: number = 12000.23;
+  precio: number = 1200; 
   constructor(private fb: FormBuilder,
               private _usuariosService: UsuariosService,
               private _empleadosService: EmpleadosService,
               private _reservacionesService: ReservacionesService,
-              // private _cookies: CookiesService,
+              private _cookies: CookiesService,
               ) 
   { 
     this.nuevoRegistro = this.fb.group({
@@ -33,7 +38,7 @@ export class LandingPageComponent implements OnInit
 
   ngOnInit(): void 
   {
-    // 
+    //
   }
 
   agregarRegistro()
@@ -53,27 +58,27 @@ export class LandingPageComponent implements OnInit
     }
 
     let idReservacion = ""+generateRandomString();
-    // if(this._cookies.checkToken())
-    // {
-    //   console.log(this.nuevoRegistro);
-    //   const nuevaRes: Reservacion = 
-    //   {
-    //     idRes: "idReservacion",
-    //     idUser: 1999,
-    //     fecIn: this.nuevoRegistro.value.fecIn,
-    //     fecOut: this.nuevoRegistro.value.fecOut,
-    //     numA: this.nuevoRegistro.value.NumA,
-    //     numN: this.nuevoRegistro.value.NumN,
-    //     numC: this.nuevoRegistro.value.NumC,
-    //   }
-    //   this._registroService.addReservacion(nuevaRes).subscribe();
+    if(this._cookies.checkToken())
+    {
+      console.log(this.nuevoRegistro);
+      const nuevaRes: Reservacion = 
+      {
+        idRes: idReservacion,
+        idUser: 1999,
+        fecIn: this.nuevoRegistro.value.fecIn,
+        fecOut: this.nuevoRegistro.value.fecOut,
+        numA: this.nuevoRegistro.value.NumA,
+        numN: this.nuevoRegistro.value.NumN,
+        numC: this.nuevoRegistro.value.NumC,
+      }
+      this._reservacionesService.addReservacion(nuevaRes).subscribe();
       
-    //   this._registroService.getReservaciones().subscribe(res =>{
-    //     console.log(res)
-    //   },
-    //     err => console.log(err)
-    //   )
-    // }
+      this._reservacionesService.getReservaciones().subscribe(res =>{
+        console.log(res)
+      },
+        err => console.log(err)
+      )
+    }
   }
 
 }
--- src/app/Pipes/precio.pipe.ts
+++ src/app/Pipes/precio.pipe.ts
@@ -5,7 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PrecioPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(precio: number, args?:any): any
+  {
+    if(args != null)
+    {
+      //SE HACE EL CAMBIO A DOLLARES
+      if(args == 'USD')
+      {
+        return precio * 20.39;
+      }
+
+      //SE HACE EL CAMBIO A EUROS
+      if(args == "EUR")
+      {
+        return precio == 23.17;
+      }
+
+      //SE HACE EL CAMBIO A YENS
+      if(args == "MXN")
+      {
+        return precio * 1;
+      }
+    }
+    else
+    {
+      return precio;
+    }
     return null;
   }
 
