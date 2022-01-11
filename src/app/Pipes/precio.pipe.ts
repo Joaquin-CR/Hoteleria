39802d3c07d16af85ae3013ce486329981985dd5
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-01-11 15:49:48 -0600

    Corrección de la lógica dentro del PIPE

--- src/app/Pipes/precio.pipe.ts
+++ src/app/Pipes/precio.pipe.ts
@@ -12,24 +12,26 @@ export class PrecioPipe implements PipeTransform {
       //SE HACE EL CAMBIO A DOLLARES
       if(args == 'USD')
       {
-        return precio * 20.39;
+        return precio / 20.39;
       }
 
       //SE HACE EL CAMBIO A EUROS
       if(args == "EUR")
       {
-        return precio == 23.17;
+        return precio * 23.17;
       }
 
       //SE HACE EL CAMBIO A YENS
+      if(args == "YEN")
+      {
+        return precio * 5.65;
+      }
+
+      //SE HACE EL CAMBIO A PESOS MEXICANOS 
       if(args == "MXN")
       {
-        return precio * 1;
+        return precio;
       }
-    }
-    else
-    {
-      return precio;
     }
     return null;
   }
