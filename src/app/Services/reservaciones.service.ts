4515b2d35d63aecfdb70810d972f6774a51f5f65
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2022-01-12 18:35:27 -0600

    Se crearon funciones en el servicio de reservaciones para guardar la información de un usuario que aún no inicia sesión

--- src/app/Services/reservaciones.service.ts
+++ src/app/Services/reservaciones.service.ts
@@ -7,6 +7,11 @@ import { Injectable } from '@angular/core';
 export class ReservacionesService {
 
   url = "/api/reservacion";
+  pendiente!: reservacionPendiente;
+
+  /* CUANDO NO HAY RESERVACION PENDIENTE FALSE, CUANDO HAY UNA RESERVACIÓN QUE SE 
+  TRAE DIRECTO DE LA LANDING ES TRUE */
+  status: boolean = false;
   
   constructor(private _http: HttpClient) 
   { 
@@ -44,12 +49,42 @@ export class ReservacionesService {
     return this._http.put(this.url+'/'+id, reservacion);
   }
 
+  reservacionPendiente(pendiente: reservacionPendiente)
+  {
+    this.pendiente = pendiente;
+  }
+
+  getResPendiente()
+  {
+    return this.pendiente;
+  }
+
+  setStatusRes(estado: boolean)
+  {
+    this.status = estado;
+  }
+
+  getStatusRes()
+  {
+    return this.status;
+  }
+
 }
 
 export interface Reservacion
 {
   idRes: string;
   idUser: number;
+  fecIn: string;
+  fecOut: string;
+  numA: number;
+  numN: number;
+  numC: number;
+}
+
+export interface reservacionPendiente
+{
+  idRes: string;
   fecIn: string;
   fecOut: string;
   numA: number;
